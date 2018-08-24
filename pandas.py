ad0f43262c7366d47e0ea0bd7e267b7fd8d3c7b7
Anjali Trimukhe <40752373+Anjalitrimukhe@users.noreply.github.com>
2018-08-24 11:44:14 +0530

    Add files via upload

--- pandas.py
+++ pandas.py
@@ -1,6 +1,8 @@
+
 import pandas as pd
+
 ds=pd.read_csv('Game_medal.csv',encoding='ISO-8859-1')
-encoding='ISO-8859-1'
+
 ds.head()
 ds.tail()
 ds.shape
@@ -8,4 +10,12 @@ ds.shift
 ds.describe()
 ds.info()
 ds.plot()
-
+ import matplotlib.pyplot as p
+ 
+ p.xlabel('records')
+ p.ylabel('year of event')
+ p.title('game_graph')
+ p.plot(ds.edition,label='year of event',color='green',linewidth=3)
+ fig=p.gcf()
+ ds.plot()
+ fid.savefig('figu.png')
